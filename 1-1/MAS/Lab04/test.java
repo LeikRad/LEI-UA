e4b3ab57ef384b5ad85875afa2d41ef2cc9bf4b0
LeikRad <48689668+LeikRad@users.noreply.github.com>
2022-03-02 00:36:46 +0000

    MAS

---
+++ 1-1/MAS/Lab04/test.java
@@ -0,0 +1,25 @@
+A preparar os dados... A gerar .. Prato [nome=Dieta n.1,0 ingredientes, preco 200.0]    
+Ingrediente 1 adicionado: Cereal [nome=Milho; Alimento [proteinas=19.3, calorias=32.4, peso=110.0]]
+Ingrediente 2 adicionado: Peixe [tipo=CONGELADO; Alimento [proteinas=31.3, calorias=25.3, peso=200.0]]
+
+A gerar .. Prato [nome=Combinado n.2,0 ingredientes, preco 100.0]    
+Ingrediente 1 adicionado: Peixe [tipo=CONGELADO; Alimento [proteinas=31.3, calorias=25.3, peso=200.0]]
+Ingrediente 2 adicionado: Legume [nome=Couve Flor; Alimento [proteinas=21.3, calorias=22.4, peso=150.0]] 
+
+A gerar .. Prato [nome=Vegetariano n.3,0 ingredientes, preco 120.0]         
+Ingrediente 1 adicionado: Cereal [nome=Milho; Alimento [proteinas=19.3, calorias=32.4, peso=110.0]]         
+Ingrediente 2 adicionado: Cereal [nome=Milho; Alimento [proteinas=19.3, calorias=32.4, peso=110.0]] 
+
+A gerar .. Prato [nome=Combinado n.4,0 ingredientes, preco 100.0]         
+Ingrediente 1 adicionado: Cereal [nome=Milho; Alimento [proteinas=19.3, calorias=32.4, peso=110.0]]         
+Ingrediente 2 adicionado: Cereal [nome=Milho; Alimento [proteinas=19.3, calorias=32.4, peso=110.0]] 
+
+Ementa para hoje: Ementa [nome=Menu Primavera, local=Loja 1, dia 2020-11-  22T21:08:45.624777300]         
+
+Dieta n.1       200.0         Combinado n.2   100.0         Vegetariano n.3 120.0         Combinado n.4   100.0 ] 
+
+Pedido gerado: Pedido: Cliente = Joao Pinto          prato: Prato [nome=Combinado n.2,2 ingredientes, preco 100.0]          
+
+prato: Prato [nome=Combinado n.2,2 ingredientes, preco 100.0] datahora=2020-11-  22T21:08:45.813778700] 
+
+Custo do Pedido: 200.0 Calorias do Pedido: 95.4 
